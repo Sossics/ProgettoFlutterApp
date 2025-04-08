4c1bad6209935e9f14b547489e723801dc0e2f7e
Effeemme06 <effeemmeco6@gmail.com>
2025-04-08 09:48:26 +0200

    ciao

---
+++ WebServiceApp/nota/edit_note_body.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Edit note body 
+ * 
+ *  Modificare il corpo di una nota
+ * 
+ *  @method PATCH
+ * 
+ *  I parametri richiesti sono:
+ *      @param mod: formato di risposta (xml o json)
+ *      @param id_note: l'id della nota da modificare
+ *      @param body: il corpo della nota
+ * 
+ *   La risposta è in formato xml o json a seconda del parametro mod e restituisce il risultato dell'operazione
+ * 
+ *  @version 1.0
+ *  @author:  Marco Favaro e Michele Russo
+ * 
+ **/
+
+require '../utils/db.php';
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+if($_SERVER["REQUEST_METHOD"] != "PATCH") {
+    http_response_code(405);
+    exit;
+}else if(verifica_token()){
+
+if (isset($data["id_note"]) && isset($data["body"]) && isset($data["mod"])) {
+    $xml = new SimpleXMLElement('<UpdateResults/>');
+
+    $sql = "UPDATE nota SET corpo = ? WHERE nota.id = ?";
+    $stmt = $conn->prepare($sql);
+
+    $stmt->bind_param("si", $data["body"], $data["id_note"]);
+
+    if ($stmt->execute()) {
+
+        $xml->addChild('success', 'true');
+        $xml->addChild('message', 'Corpo della nota aggiornato con successo!');
+        $xml->addChild('error', '');
+        http_response_code(200);
+    } else {
+        $xml->addChild('success', 'false');
+        $xml->addChild('message', 'Nota non aggiornata!');
+        $xml->addChild('error', 'Error during the updating');
+        http_response_code(401);
+    }
+    $stmt->close();
+
+    if ($data["mod"] == "xml") {
+        header('Content-Type: application/xml');
+        echo $xml->asXML();
+    } else if ($data["mod"] == "json") {
+        header('Content-Type: application/json');
+        echo json_encode($xml);
+    }
+} else {
+    http_response_code(400);
+}
+
+}else{
+    http_response_code(401);
+}
+
+?>
