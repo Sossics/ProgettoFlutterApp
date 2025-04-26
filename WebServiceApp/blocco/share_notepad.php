0bdc4a6597db2a0baf0549a2e4e4444aaed79fb3
Effeemme06 <effeemmeco6@gmail.com>
2025-04-26 11:24:48 +0200

    share

--- WebServiceApp/blocco/share_notepad.php
+++ WebServiceApp/blocco/share_notepad.php
@@ -1,15 +1,15 @@
 <?php
 
 /**
- * Share note
+ * Share notepad
  * 
- *  Condivide una nota con un altro utente
+ *  Condivide un blocco con un altro utente
  * 
  *  @method GET
  * 
  *  I parametri richiesti sono:
  *      @param mod: formato di risposta (xml o json)
- *      @param id_note: id della nota da condividere
+ *      @param id_notepad: id della nota da condividere
  *      @param email: email dell'utente con cui condividere la nota
  *      @param permission: permesso di condivisione (1 = lettura, 2 = scrittura)
  * 
@@ -27,7 +27,7 @@ if($_SERVER["REQUEST_METHOD"] != "GET") {
     exit;
 }else if(verifica_token()){
 
-if (isset($_GET["id_note"]) && isset($_GET["mod"]) && isset($_GET["email"]) && isset($_GET["permission"])) {
+if (isset($_GET["id_notepad"]) && isset($_GET["mod"]) && isset($_GET["email"]) && isset($_GET["permission"])) {
     $xml = new SimpleXMLElement('<result/>');
 
     $sql = "SELECT * FROM utente WHERE email = ?";
@@ -43,23 +43,42 @@ if (isset($_GET["id_note"]) && isset($_GET["mod"]) && isset($_GET["email"]) && i
 
         $id_utente = $row["id"];
 
+        $sql1 = "SELECT id FROM nota WHERE id_blocco = ?";
+        $stmt1 = $conn->prepare($sql1);
+        $stmt1->bind_param("i", $_GET["id_notepad"]);
 
-        $sql2 = "INSERT INTO appartiene (id_utente, id_nota, permesso) VALUES (?, ?, ?)";
-        $stmt2 = $conn->prepare($sql2);
-        $stmt2->bind_param("iii", $id_utente, $_GET["id_note"], $_GET["permission"]);
+        if($stmt1->execute()){
 
-        if($stmt2->execute()){
+            $result1 = $stmt1->get_result();
 
-            $xml->addChild('success', 'true');
-            $xml->addChild('message', 'Nota condivisa con successo');
-            $xml->addChild('error', '');
-            http_response_code(200);
+            if ($result1->num_rows > 0) {
+                
+                $sql2 = "INSERT INTO appartiene (id_utente, id_nota, permesso) VALUES (?, ?, ?)";
+                $stmt2 = $conn->prepare($sql2);
+                $stmt2->bind_param("iii", $id_utente, $id_nota, $_GET["permission"]);
+
+                while ($row1 = $result1->fetch_assoc()) {
+                    $id_nota = $row1["id"];
+                    $stmt2->execute();
+                }
+
+                $xml->addChild('success', 'true');
+                $xml->addChild('message', 'Blocco condiviso con successo!');
+                $xml->addChild('error', '');
+                http_response_code(200);
+
+            }else{
+                $xml->addChild('success', 'false');
+                $xml->addChild('message', 'Blocco non trovato!');
+                $xml->addChild('error', 'Error during the share notepad operation');
+                http_response_code(404);
+            }
 
         }else{
 
             $xml->addChild('success', 'false');
             $xml->addChild('message', 'Errore durante la condivisione');
-            $xml->addChild('error', 'Error during the share note operation');
+            $xml->addChild('error', 'Error during the share notepad operation');
             http_response_code(400);
 
         }
@@ -69,7 +88,7 @@ if (isset($_GET["id_note"]) && isset($_GET["mod"]) && isset($_GET["email"]) && i
 
         $xml->addChild('success', 'false');
         $xml->addChild('message', 'Utente non trovato!');
-        $xml->addChild('error', 'Error during the share note operation');
+        $xml->addChild('error', 'Error during the share notepad operation');
         http_response_code(404);
     }
     $stmt->close();
