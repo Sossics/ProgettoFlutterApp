1d409c2283eb29814c98f9d4417a874b8134cd6e
Sossics <michelerusso267@gmail.com>
2025-05-05 23:29:22 +0200

    Added noteblock handling

--- flutter_application_app/lib/Provider/AppProvider.dart
+++ flutter_application_app/lib/Provider/AppProvider.dart
@@ -8,64 +8,103 @@ class AppProvider with ChangeNotifier {
   final ApiService _apiService = ApiService();
   final StorageService _StorageService = StorageService();
 
-  String? mod;
-   String? _token;
-  int userID = -1;
+  String? _mod;
+  String? _token;
+  int _userID = -1;
 
   AppProvider() {
     _initialize();
   }
 
   Future<void> _initialize() async {
-    mod = await _StorageService.getMod();
+    _mod = await _StorageService.getMod();
+    _token = await _StorageService.getToken();
     if (_token != null) {
       Map<String, dynamic> decodedToken = JwtDecoder.decode(_token!);
-      userID = decodedToken['uid'] ?? -1;
-      print("\n\n-------------------------------------------" + userID.toString());
+      _userID = decodedToken['uid'] ?? -1;
+      print(
+        "\n\n-------------------------------------------" + _userID.toString(),
+      );
     }
     await _apiService.ready;
   }
 
-
   // Variabili per lo stato
   List<Map<String, dynamic>> _notes = [];
+  List<Map<String, dynamic>> _notepads = [];
+
   bool _isLoading = true;
   bool _hasError = false;
 
   List<Map<String, dynamic>> get notes => _notes;
+  List<Map<String, dynamic>> get notepads => _notepads;
+
   bool get isLoading => _isLoading;
   bool get hasError => _hasError;
 
   // ==================== NOTEPAD ====================
 
-  Future<List<dynamic>?> fetchNotepads() async {
-    print("Fetching notepads...");
-    print("Using Endpoint: ${NotelyApiConstants.FETCH_NOTEPAD}");
-    final response = await _apiService.getRequest(NotelyApiConstants.FETCH_NOTEPAD + "?mod=$mod");
-    if (response != null && response['success'] == 'true') {
-      return response['notepads'];
+  Future<void> fetchNotepads() async {
+    _isLoading = true;
+    _hasError = false;
+    notifyListeners();
+
+    try {
+      print("Fetching notepads...");
+      print("Using Endpoint: ${NotelyApiConstants.FETCH_NOTEPAD}");
+
+      final response = await _apiService.getRequest(
+        "${NotelyApiConstants.FETCH_NOTEPAD}?mod=$_mod&id_user=$_userID",
+      );
+
+      if (response != null && response['success'] == 'true') {
+        var data = response['notepads'];
+
+        if (data is List) {
+          _notepads = List<Map<String, dynamic>>.from(data);
+        } else if (data is Map<String, dynamic>) {
+          _notepads = [data];
+        } else {
+          _notepads = [];
+        }
+      } else {
+        _hasError = true;
+      }
+    } catch (e) {
+      print("Errore in fetchNotepads: $e");
+      _hasError = true;
     }
-    return null;
+
+    _isLoading = false;
+    notifyListeners();
   }
 
   Future<bool> createNotepad(String title) async {
     print("Creating notepad with title: $title");
     print("Using Endpoint: ${NotelyApiConstants.CREATE_NOTEPAD}");
-    final response = await _apiService.postRequest(NotelyApiConstants.CREATE_NOTEPAD, {"title": title});
+    final response = await _apiService.postRequest(
+      NotelyApiConstants.CREATE_NOTEPAD,
+      {"title": title},
+    );
     return response != null && response['success'] == 'true';
   }
 
   Future<bool> editNotepadBody(int idNotepad, String newBody) async {
     print("Editing notepad with ID: $idNotepad");
     print("Using Endpoint: ${NotelyApiConstants.EDIT_NOTEPAD_BODY}");
-    final response = await _apiService.putRequest(NotelyApiConstants.EDIT_NOTEPAD_BODY, {"id": idNotepad, "body": newBody});
+    final response = await _apiService.putRequest(
+      NotelyApiConstants.EDIT_NOTEPAD_BODY,
+      {"id": idNotepad, "body": newBody},
+    );
     return response != null && response['success'] == 'true';
   }
 
   Future<bool> deleteNotepad(int idNotepad) async {
     print("Deleting notepad with ID: $idNotepad");
     print("Using Endpoint: ${NotelyApiConstants.DELETE_NOTEPAD}");
-    final response = await _apiService.deleteRequest("${NotelyApiConstants.DELETE_NOTEPAD}/$idNotepad");
+    final response = await _apiService.deleteRequest(
+      "${NotelyApiConstants.DELETE_NOTEPAD}/$idNotepad",
+    );
     return response != null && response['success'] == 'true';
   }
 
@@ -74,54 +113,68 @@ class AppProvider with ChangeNotifier {
   Future<bool> createNote(int idNotepad, String title, String body) async {
     print("Creating note in notepad with ID: $idNotepad");
     print("Using Endpoint: ${NotelyApiConstants.CREATE_NOTE}");
-    final response = await _apiService.postRequest(NotelyApiConstants.CREATE_NOTE, {"id_notepad": idNotepad, "title": title, "body": body});
+    final response = await _apiService.postRequest(
+      NotelyApiConstants.CREATE_NOTE,
+      {"id_notepad": idNotepad, "title": title, "body": body},
+    );
     return response != null && response['success'] == 'true';
   }
 
   Future<bool> editNoteTitle(int idNote, String newTitle) async {
     print("Editing note with ID: $idNote");
     print("Using Endpoint: ${NotelyApiConstants.EDIT_NOTE_TITLE}");
-    final response = await _apiService.patchRequest(NotelyApiConstants.EDIT_NOTE_TITLE, {"id": idNote, "title": newTitle});
+    final response = await _apiService.patchRequest(
+      NotelyApiConstants.EDIT_NOTE_TITLE,
+      {"id": idNote, "title": newTitle},
+    );
     return response != null && response['success'] == 'true';
   }
 
   Future<bool> editNoteBody(int idNote, String newBody) async {
     print("Editing note with ID: $idNote");
     print("Using Endpoint: ${NotelyApiConstants.EDIT_NOTE_BODY}");
-    final response = await _apiService.patchRequest(NotelyApiConstants.EDIT_NOTE_BODY, {"id": idNote, "body": newBody});
+    final response = await _apiService.patchRequest(
+      NotelyApiConstants.EDIT_NOTE_BODY,
+      {"id": idNote, "body": newBody},
+    );
     return response != null && response['success'] == 'true';
   }
 
   Future<bool> shareNote(int idNote, String username) async {
     print("Sharing note with ID: $idNote to user: $username");
     print("Using Endpoint: ${NotelyApiConstants.SHARE_NOTE}");
-    final response = await _apiService.postRequest(NotelyApiConstants.SHARE_NOTE, {"id_note": idNote, "username": username});
+    final response = await _apiService.postRequest(
+      NotelyApiConstants.SHARE_NOTE,
+      {"id_note": idNote, "username": username},
+    );
     return response != null && response['success'] == 'true';
   }
 
   Future<bool> deleteNote(int idNote) async {
     print("Deleting note with ID: $idNote");
     print("Using Endpoint: ${NotelyApiConstants.DELETE_NOTE}");
-    final response = await _apiService.deleteRequest("${NotelyApiConstants.DELETE_NOTE}/$idNote");
+    final response = await _apiService.deleteRequest(
+      "${NotelyApiConstants.DELETE_NOTE}/$idNote",
+    );
     return response != null && response['success'] == 'true';
   }
 
   Future<void> fetchNotes({int? idNotepad}) async {
-  _isLoading = true;
-  _hasError = false;
-  notifyListeners();
+    _isLoading = true;
+    _hasError = false;
+    notifyListeners();
 
-  try {
-    String url = NotelyApiConstants.NoteEndpoint;
+    try {
+      String url = NotelyApiConstants.NoteEndpoint;
 
-    if (idNotepad != null) {
-      url += "?id_notepad=$idNotepad";
-    }
-    if (mod != null) {
-      url += (idNotepad != null ? "&" : "?") + "mod=$mod";
-    }
+      if (idNotepad != null) {
+        url += "?id_notepad=$idNotepad";
+      }
+      if (_mod != null) {
+        url += (idNotepad != null ? "&" : "?") + "mod=$_mod";
+      }
 
-    final response = await _apiService.getRequest(url);
+      final response = await _apiService.getRequest(url);
 
       if (response != null && response['success'] == 'true') {
         var notesData = response['notes']['Note'];
